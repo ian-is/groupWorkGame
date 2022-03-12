851ed050b34815cf65fe2d1bcf167207d3a6015f
ginoross <gr211714@reigate.ac.uk>
2022-03-12 22:15:42 +0000

    game works, no terrain gen or start/end screen yet

--- src/main/java/com/example/groupworkgame/Game.java
+++ src/main/java/com/example/groupworkgame/Game.java
@@ -5,97 +5,99 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 
 public class Game extends Application {
 
-    Player player = new Player(false,0, 0, false);
-    boolean up;
+    Player player = new Player(false, 0, 0, false);
+    int counter;
+    int score;
+
 
     @Override
     public void start(Stage stage) throws IOException {
 
-
         FXMLLoader fxmlLoader = new FXMLLoader(Game.class.getResource("game-scene.fxml"));
-
 
         Parent root = fxmlLoader.load();
         GameController controller = fxmlLoader.getController();
         Scene scene = new Scene(root);
         stage.setFullScreen(false);
 
-
-        //minor issue with input registration but will sort it in class
-
         scene.setOnMousePressed(mouseEvent -> player.setUp(mouseEvent.isPrimaryButtonDown()));
-        scene.setOnMouseReleased(mouseEvent -> player.setUp(false));
-        scene.setOnMousePressed(mouseEvent -> up = mouseEvent.isPrimaryButtonDown());
-        scene.setOnMousePressed(mouseEvent -> up = false);
 
+        scene.setOnMouseReleased(mouseEvent -> player.setUp(false));
 
         stage.setScene(scene);
+
+        for (int i = 0; i < root.getChildrenUnmodifiable().size(); i++) {
+            if (root.getChildrenUnmodifiable().get(i).getId().contains("background")) {
+                controller.backgrounds.add((ImageView) root.getChildrenUnmodifiable().get(i));
+            }
+        }
+
+        for (int i = 0; i < root.getChildrenUnmodifiable().size(); i++) {
+            if (root.getChildrenUnmodifiable().get(i).getId().contains("obj")) {
+                controller.objects.add((ImageView) root.getChildrenUnmodifiable().get(i));
+            }
+        }
+
+        controller.initialiseObjects();
+
         stage.show();
-
-
 
 
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
-
-
-                if(!player.isDead){
+                if (!player.isDead) {
+                    if (counter % 5 == 0) {
+                        score++;
+                    }
+                    switch (counter) {
+                        case 50 -> controller.createTopLine();
+                        case 100 -> {
+                            controller.createBottomLine();
+                            counter = 0;
+                        }
+                    }
+                    counter++;
+                    controller.cycleBackground();
+                    controller.moveObjects();
                     updatePlayerPos(player.isUp(), player.getVelY(), player.getRotY());
+                    if (controller.checkCollisions()) {
+                        System.out.println("you got: " + score);
+                        player.setDead(true);
+                    }
                 }
-
-
-
             }
-
-
-
 
             private void updatePlayerPos(boolean up, float velY, float rotY) {
 
-                if(controller.y - 2*velY >= 0 && controller.y - 2*velY < 1020){
+                if (controller.y - 2 * velY >= 0 && controller.y - 2 * velY < 1020) {
                     if (up && velY + 0.30 < 12) velY += 0.30;
                     if (!up && velY - 0.17 > -10) velY -= 0.17;
-                    rotY = 4*-velY;
+                    rotY = 4 * -velY;
                 }
-                if (controller.y - 2*velY < 0){
+                if (controller.y - 2 * velY < 0) {
                     velY = 0;
 
                 }
-                if (controller.y - 2*velY > 1020){
-                    velY = 0 ;
+                if (controller.y - 2 * velY > 1020) {
+                    velY = 0;
                 }
-
-                //check collisions here
-
                 controller.movePlayer(velY, rotY);
                 player.setRotY(rotY);
                 player.setVelY(velY);
-
-
             }
-
-
         };
-
         timer.start();
-
-
-
     }
-
-
-
 
     public static void main(String[] args) {
         launch();
     }
 }
-
-
-
--- src/main/java/com/example/groupworkgame/GameController.java
+++ src/main/java/com/example/groupworkgame/GameController.java
@@ -1,30 +1,105 @@
 package com.example.groupworkgame;
 
-
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.layout.AnchorPane;
+
+import java.util.ArrayList;
 
 public class GameController {
     @FXML
+    private ImageView background;
+    @FXML
+    private ImageView background2;
+    @FXML
     private ImageView playerSprite;
-    public double y = 200;
+    @FXML
+    private AnchorPane screen;
+    public double y;
+    @FXML
+    public ArrayList<ImageView> objects = new ArrayList<>();
+    public ArrayList<ImageView> backgrounds = new ArrayList<>();
 
-    public int playerWidth = 100;
-    public int playerHeight = 100;
-    public int playerX = 900;
-    public double playerY = y;
+    public void initialiseObjects() {
+        playerSprite.setX(900);
+        for (int i = 0; i < objects.size(); i++) {
+            objects.get(i).setX(-500);
+        }
+        background.setX(0);
+        background2.setX(1919);
+    }
 
     public void movePlayer(float velY, float rotY) {
-        y-=velY;
+        y -= velY;
         playerSprite.setY(y);
         playerSprite.setRotate(rotY);
 
     }
 
+    public void moveObjects() {
+        for (int i = 0; i < objects.size(); i++) {
+            if (isOnScreen(objects.get(i), screen)) {
+                objects.get(i).setX(objects.get(i).getX() - 5);
+            }
+        }
+    }
 
+    public void cycleBackground() {
+        for (int i = 0; i < backgrounds.size(); i++) {
+            if (!isOnScreen(backgrounds.get(i), screen)) {
+                backgrounds.get(i).setX(1919);
+            } else {
+                backgrounds.get(i).setX(backgrounds.get(i).getX() - 5);
+            }
+        }
+    }
 
+    public void createTopLine() {
+        for (int i = 0; i < objects.size(); i++) {
+            if (!isOnScreen(objects.get(i), screen)) {
+                objects.get(i).setX(1919);
+                objects.get(i).setY(0);
+                objects.get(i).setRotate(0);
+                return;
+            }
+        }
+    }
 
+    public void createBottomLine() {
+        for (int i = 0; i < objects.size(); i++) {
+            if (!isOnScreen(objects.get(i), screen)) {
+                objects.get(i).setX(1919);
+                objects.get(i).setY(screen.getPrefHeight() - objects.get(i).getFitHeight());
+                objects.get(i).setRotate(180);
+                return;
+            }
+        }
+    }
+
+    public boolean checkCollisions() {
+        for (int i = 0; i < objects.size(); i++) {
+            if (isOnScreen(objects.get(i), screen)) {
+                if (collidesWith(playerSprite, objects.get(i))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean collidesWith(ImageView one, ImageView two) {
+        return one.getX() < two.getX() + two.getFitWidth() &&
+                one.getX() + one.getFitWidth() > two.getX() &&
+                one.getY() < two.getY() + two.getFitHeight() &&
+                one.getFitHeight() + one.getY() > two.getY();
+    }
+
+    public boolean isOnScreen(ImageView one, AnchorPane two) {
+        return one.getX() < two.getPrefWidth() &&
+                one.getX() + one.getFitWidth() > 0 &&
+                one.getY() < two.getPrefHeight() &&
+                one.getFitHeight() + one.getY() > 0;
+    }
 
 }
 
